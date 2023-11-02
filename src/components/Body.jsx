3f2451ea921e131a9a91d1149cc6edb692d047bb
Jean-Francois <hafidjeanfrancois@gmail.com>
2023-11-02 16:58:38 +0100

    bug test

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,11 +1,20 @@
 import PropTypes from "prop-types";
-import "./Body.css";
+import "./Body.scss";
 
-function Body({ list }) {
+function Body({ list, setCounter }) {
+  const handleClick = () => {
+    setCounter((counter) => counter + 1);
+  };
   function filtermap(filter, filter2) {
     return (
       <section>
-        <h1 className="title">Nains et Gnomes</h1>
+        {filter2 ? (
+          <h1 className="title">
+            {filter} & {filter2}
+          </h1>
+        ) : (
+          <h1 className="title">{filter}</h1>
+        )}
         <div className="block">
           {list &&
             list
@@ -27,7 +36,9 @@ function Body({ list }) {
                     <p>Description: {l.description}</p>
                     <p>category: {l.category}</p>
                   </div>
-                  <button type="button">Add to Cart</button>
+                  <button type="button" onClick={handleClick}>
+                    Add to Cart
+                  </button>
                 </div>
               ))}
         </div>
@@ -44,6 +55,7 @@ function Body({ list }) {
 }
 
 Body.propTypes = {
+  setCounter: PropTypes.func.isRequired,
   list: PropTypes.array.isRequired,
 };
 
